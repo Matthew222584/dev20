c40d869d0a980fdb71c408ff033cce29274263e9
Matthew222584 <114376634+Matthew222584@users.noreply.github.com>
2023-11-06 16:55:23 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,9 +1,25 @@
 var video;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+	console.log("Good job opening the window");
+	videoThing = document.getElementById("player1");
+	videoThing.autoplay = false;
+	videoThing.loop = false;
 
 });
+
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause")
+const volume = document.getElementById('slider');
+
+playButton.addEventListener('click', function() {
+	videoThing.play();
+});
+
+pauseButton.addEventListener('click', function() {
+	videoThing.pause();
+});
+
 
 // document.querySelector("#play").addEventListener("click", function() {
 // 	console.log("Play Video");
